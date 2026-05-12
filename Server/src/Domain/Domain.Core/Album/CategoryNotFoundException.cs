080ecb1cb9919baa342cb606d17aad4ffdfec4fc
Jagdender <jagdender@outlook.com>
2026-05-13 05:58:34 +0800

    Update Alias value in CategoryNotFoundException class
    Changed the Alias attribute from "exception_category_not_found" to "category_not_found_exception" for consistency and clarity.

--- Server/src/Domain/Domain.Core/Album/CategoryNotFoundException.cs
+++ Server/src/Domain/Domain.Core/Album/CategoryNotFoundException.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Album;
 
-[Alias("exception_category_not_found")]
+[Alias("category_not_found_exception")]
 [GenerateSerializer(GenerateFieldIds = GenerateFieldIds.PublicProperties)]
 internal sealed class CategoryNotFoundException(CategoryId id)
     : Exception($"Category with id {id} not found.")
